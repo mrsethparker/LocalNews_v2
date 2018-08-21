b0bbf05cd48f72b8d7ad3f3c8ccf5e96ba79dcc5
mrsethparker <36980118+mrsethparker@users.noreply.github.com>
2018-08-20 22:12:00 -0700

    feat: implement a preference listener

--- app/src/main/java/com/example/android/localnews/SettingsActivity.java
+++ app/src/main/java/com/example/android/localnews/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.localnews;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -12,11 +15,37 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment {
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            //obtain the settings preference so we can change the summary
+            Preference searchTerm = findPreference(getString(R.string.settings_search_term_key));
+
+            //bind the preference summary to a value
+            bindPreferenceSummaryToValue(searchTerm);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            //update the displayed preference after it has been changed
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            //set the preference listener to the current activity
+            preference.setOnPreferenceChangeListener(this);
+
+            //get the current value of the preference and update the summary
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
